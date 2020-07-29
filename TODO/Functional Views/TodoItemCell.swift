4e1c1e6e85aa9c3b04cb8c6cd452e8b324f4689f
AbanoubAshraf <Abanoub.zaki@blink22.com>
2020-07-29 18:10:41 +0200

    supporting files

---
+++ TODO/Functional Views/TodoItemCell.swift
@@ -0,0 +1,26 @@
+//
+//  TodoItemCell.swift
+//  TODO
+//
+//  Created by Blink22 on 7/27/20.
+//  Copyright © 2020 Blink22. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class TodoItemCell: UITableViewCell {
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
+    var todo: TodoItem? {
+      didSet {
+        guard let todo = todo else { return }
+
+        nameLabel.text = todo.name
+        descriptionLabel.text = todo.description
+      }
+    }
+}
